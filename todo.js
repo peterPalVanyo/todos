48446231e57ebd29aea555555d5bdc8537704c52
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-10-16 17:28:52 +0200

    basic html and todos rendering

--- todo.js
+++ todo.js
@@ -7,9 +7,68 @@ const todos = [{
 },  {
     text: "Complete the code",
     completed: false
+},  {
+    text: "Walk with the dog",
+    completed: false
+},  {
+    text: "Feed the cat",
+    completed: true
+},  {
+    text: "Meeting with Samantha Fox",
+    completed: false
 }]
+const filters = {
+    searchText: ''
+}
 
-const deleteTodo = function(todos, todo) {
+const body = document.querySelector('body')
+const button = document.querySelector('#addTodo')
+const inputFilter = document.querySelector('#inputFilter')
+const todosEl = document.querySelector('#todos')
+
+
+const renderTodos = function(todos, filters) {
+    let filteredTodos = todos.filter((todo) => todo.text.toLocaleLowerCase().includes(filters.searchText))
+    filteredTodos.forEach(function(todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        todosEl.appendChild(p)
+    })
+
+    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
+    
+    const h2 = document.createElement('h2')
+    h2.textContent = `You have ${incompleteTodos.length} todos left`
+    todosEl.appendChild(h2)
+}
+
+renderTodos(todos, filters)
+
+inputFilter.addEventListener('input', function(e) {
+    filters.searchText = e.target.value.toLowerCase()
+    //clear the div
+    todosEl.innerHTML = ''
+    renderTodos(todos, filters)
+})
+
+//click
+button.addEventListener('click', function(){
+    console.log('hello')
+})
+
+
+
+
+/* todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    body.appendChild(p)
+}) */
+
+
+
+
+/* const deleteTodo = function(todos, todo) {
     const index = todos.findIndex(function (item, index) {
         return item.text.toLowerCase() === todo.toLowerCase()
     })
@@ -31,13 +90,7 @@ const sortTodos = function(todos) {
             return 1
         } else {return 0}
     })
-}
-
-const thingsTodo = getThingsTodo(todos, true)
-
-sortTodos(todos)
-console.log(todos)
-
+} */
 
 /* 
 todos.forEach(function(note, index) {

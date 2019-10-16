291d98aa0929da030874c218bacae93948b57c30
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-10-16 18:31:56 +0200

    can add new todo to todos

--- todo.js
+++ todo.js
@@ -28,6 +28,7 @@ const todosEl = document.querySelector('#todos')
 
 
 const renderTodos = function(todos, filters) {
+    todosEl.innerHTML = ''
     let filteredTodos = todos.filter((todo) => todo.text.toLocaleLowerCase().includes(filters.searchText))
     filteredTodos.forEach(function(todo) {
         const p = document.createElement('p')
@@ -46,56 +47,18 @@ renderTodos(todos, filters)
 
 inputFilter.addEventListener('input', function(e) {
     filters.searchText = e.target.value.toLowerCase()
-    //clear the div
-    todosEl.innerHTML = ''
     renderTodos(todos, filters)
 })
 
-//click
-button.addEventListener('click', function(){
-    console.log('hello')
+
+button.addEventListener('submit', function(e){
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    e.target.elements.text.value = ''
+    renderTodos(todos, filters)
 })
-
-
-
-
-/* todos.forEach(function(todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    body.appendChild(p)
-}) */
-
-
-
-
-/* const deleteTodo = function(todos, todo) {
-    const index = todos.findIndex(function (item, index) {
-        return item.text.toLowerCase() === todo.toLowerCase()
-    })
-    if(index != -1) todos.splice(index, 1)
-}
-
-const getThingsTodo = function(todos) {
-    return todos.filter(function(todo) {
-        return !todo.completed
-    })
-}
-
-const sortTodos = function(todos) {
-    todos.sort(function(a, b) {
-        //or !a.completed && b.completed
-        if(a.completed < b.completed) {
-            return -1
-        } else if(a.completed > b.completed) {
-            return 1
-        } else {return 0}
-    })
-} */
-
-/* 
-todos.forEach(function(note, index) {
-    console.log(`${index+1}. ${note}!`)
-}) */
-
 
 

3838fcbc2646feefd208e56c6bd69882a5ef4894
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-10-16 11:25:39 +0200

    deleteTodo function

--- todo.js
+++ todo.js
@@ -1,5 +1,26 @@
-const todos = ["todo1", "todo2", "todo3"]
+const todos = [{
+    text: "Do the shopping",
+    completed: false
+},  {
+    text: "Fix the kitchen furniture",
+    completed: false
+},  {
+    text: "Complete the code",
+    completed: true
+}]
 
+const deleteTodo = function(todos, todo) {
+    const index = todos.findIndex(function (item, index) {
+        return item.text.toLowerCase() === todo.toLowerCase()
+    })
+    if(index != -1) todos.splice(index, 1)
+}
+/* 
 todos.forEach(function(note, index) {
     console.log(`${index+1}. ${note}!`)
-})
+}) */
+console.log(todos)
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>")
+deleteTodo(todos, 'do the shopping')
+console.log(todos)
+
